0a3aea502775cdfa1ef9375450d1e81b1ebbbcc2
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-05 12:28:48 +0530

    Making nativewind error free process going on

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <SafeAreaView>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+    </SafeAreaView>
+  );
+}
